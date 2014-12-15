ad17aa7dc2079d07a8196d26bf655484a5dc9e09
Julian Easterling <julian@julianscorner.com>
2014-12-15 13:12:19 -0500

    Initial Commit

---
+++ AuthenticationServer.Web/Properties/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AuthenticationServer.Web")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyProduct("AuthenticationServer.Web")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("1cb047e4-4d06-4b89-8e4d-11ee87f6fa9f")]
---
+++ README.md
@@ -0,0 +1,4 @@
+Authentication Server
+=============
+
+This project contains an implementation of Identity Server V3 which will be responsible for providing access tokens to any authorized applications that need a single-sign on solution. The initial implementation will have in-memory users but can be changed later to use users/passwords from other sources such as SQL DB and Active Directory.
