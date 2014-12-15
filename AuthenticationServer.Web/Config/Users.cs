5b20fc3c6f2b4b47b3f120a9550eb5700adfc2d9
Julian Easterling <julian@julianscorner.com>
2014-12-15 15:06:23 -0500

    Added Test Users.

---
+++ AuthenticationServer.Web/Config/Users.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Web;
+using Thinktecture.IdentityServer.Core;
+using Thinktecture.IdentityServer.Core.Models;
+using Thinktecture.IdentityServer.Core.Services.InMemory;
+
+namespace AuthenticationServer.Web.Config
+{
+    /// <summary>
+    /// Class containing users that can authenticate
+    /// </summary>
+    /// <remarks>Currently contains "test" users.</remarks>
+    public static class Users
+    {
+        /// <summary>
+        /// Gets a list of authorized users.
+        /// </summary>
+        /// <returns>A list of authorized users</returns>
+        public static IEnumerable<InMemoryUser> Get()
+        {
+            return new[]
+                {
+                    new InMemoryUser
+                        {
+                            Username = "coordinator1",
+                            Password = "Password1",
+                            Subject = "1",
+                            Claims = new []
+                                {
+                                    new Claim(Constants.ClaimTypes.GivenName, "Coodinator"),
+                                    new Claim(Constants.ClaimTypes.FamilyName, "One") 
+                                }
+                        },
+                    new InMemoryUser
+                        {
+                            Username = "coordinator2",
+                            Password = "Password2",
+                            Subject = "1",
+                            Claims = new []
+                                {
+                                    new Claim(Constants.ClaimTypes.GivenName, "Coodinator"),
+                                    new Claim(Constants.ClaimTypes.FamilyName, "Two") 
+                                }
+                        },
+                    new InMemoryUser
+                        {
+                            Username = "admin",
+                            Password = "Pa$$w0rd",
+                            Subject = "1",
+                            Claims = new []
+                                {
+                                    new Claim(Constants.ClaimTypes.GivenName, "System"),
+                                    new Claim(Constants.ClaimTypes.FamilyName, "Administrator") 
+                                }
+                        }
+                };
+        }
+    }
+}
